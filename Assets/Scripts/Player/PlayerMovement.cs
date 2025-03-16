87f5e78955402a2febb1e92d9f0ac58105d6b464
popoyo07 <154477545+popoyo07@users.noreply.github.com>
2025-03-16 18:31:14 -0400

    third person camera still in progress

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    [Header("Movement")]
+    public float moveSpeed;
+    public float groundDrag;
 
-    public float speed;
+    public float jumpForce;
+    public float jmpCooldown;
+    public float airMulti;
+    public bool readyToJump = true;
 
-    private void Update()
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask ground;
+    bool isGround;
+
+    public Transform orientation;
+
+    float hInput;
+    float vInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+    void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+    void Update()
+    {
+        // ground check 
+        isGround = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
 
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3 (horizontal, 0f, vertical);
+        playerInputs();
+
+        // handle drag
+        if (isGround)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+            rb.drag = 0;
+    }
+    void FixedUpdate()
+    {
+        movePlayer();
+    }
+
+    void playerInputs()
+    {
+        vInput = Input.GetAxis("Vertical");
+        hInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.Space) && readyToJump && isGround)
+        {
+            readyToJump = false;
+            Jump();
+
+            Invoke(nameof(resetJump), jmpCooldown);
+        }
+    }
+
+    void movePlayer()
+    {
+        moveDirection = orientation.forward * vInput + orientation.right * hInput;
+        if (isGround)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        } else if (!isGround)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMulti, ForceMode.Force);
+
+        }
+    }
+   
+    void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void resetJump()
+    {
+        readyToJump = true;   
     }
 }
--- Assets/Scripts/Player/ThirdPerson.cs
+++ Assets/Scripts/Player/ThirdPerson.cs
@@ -12,6 +12,10 @@ public class ThirdPerson : MonoBehaviour
 
     public float rotationSpeed;
 
+    public Transform combatLookAtP;
+
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,20 +26,39 @@ public class ThirdPerson : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // rotate orientation
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
-       orientation.forward = viewDir.normalized;
 
-        // rotate playe object
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        // Get input for movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        if (inputDir != Vector3.zero)
+        // If there is input (movement keys), rotate the player
+        if (horizontalInput != 0 || verticalInput != 0)
         {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            // Calculate direction relative to camera orientation
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
+            // If movement input direction is non-zero, rotate player
+            if (inputDir != Vector3.zero)
+            {
+                // Rotate the player object smoothly
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+            }
+
+            // Optional: Add force for movement
+            Vector3 moveDir = inputDir.normalized * Time.deltaTime * 5f; // Adjust speed here
+            rb.MovePosition(rb.position + moveDir);
         }
-        
+
+        // Rotate the camera to face the player
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        // Combat look-at logic
+        if (combatLookAtP != null)
+        {
+            Vector3 dirToCombatLookAt = combatLookAtP.position - new Vector3(transform.position.x, combatLookAtP.position.y, transform.position.z);
+            orientation.forward = dirToCombatLookAt.normalized;
+            playerObj.forward = dirToCombatLookAt.normalized;
+        }
     }
 }
