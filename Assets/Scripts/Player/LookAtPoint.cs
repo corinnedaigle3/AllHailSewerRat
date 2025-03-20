eef9e028b858f174a05a6312fabffb1f17d21b2d
popoyo07 <piwedaa@gmail.com>
2025-03-19 23:50:46 -0400

    FIXED PLAYER PROJECTILE AND ROTATION

--- Assets/Scripts/Player/LookAtPoint.cs
+++ Assets/Scripts/Player/LookAtPoint.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LookAtPoint : MonoBehaviour
 {
-    public Camera cam;
+    private GameObject cam;
     public float distanceFromCamera = 5f;
+
+    private void Awake()
+    {
+        cam = GameObject.Find("MainCamera");
+    }
 
     void Update()
     {
@@ -13,8 +19,16 @@ public class LookAtPoint : MonoBehaviour
         Vector3 oppositeSide = cam.transform.forward;
 
         // Position the point at a fixed distance from the camera, parallel to its view
-        transform.position = cam.transform.position + oppositeSide * distanceFromCamera;
+        transform.position = new Vector3(
+            cam.transform.position.x + oppositeSide.x * distanceFromCamera,
+            transform.position.y,
+            cam.transform.position.z + oppositeSide.z * distanceFromCamera);
 
-        Debug.Log(oppositeSide);
+        // Rotate according to camera's forward vector
+        Vector3 flatForward = new Vector3(cam.transform.forward.x, 0f, cam.transform.forward.z).normalized;
+        if (flatForward != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(flatForward, Vector3.up);
+        }
     }
 }
--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -1,29 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Windows;
 
 public class Projectile : MonoBehaviour
 {
     Rigidbody rb;
-    public GameObject lookAtPoint;
+    private GameObject lookAtPoint;
     public float shootingForce = 1000f;
 
     private void Awake()
     {
+        lookAtPoint = GameObject.Find("LookAtPoint");
         rb = GetComponent<Rigidbody>();
     }
 
     void Start()
     {
+        Debug.Log("Look at point position: " + lookAtPoint.transform.position);
+        Debug.Log("The look at Postion " + lookAtPoint.transform.position + " and The transform position " + transform.position);
         // Get the camera's forward direction and apply force to the projectile
-        Vector3 shootingDirection = (lookAtPoint.transform.position - transform.position).normalized;
+        Vector3 shootingDirection = lookAtPoint.transform.forward;
 
-        rb.AddForce(shootingDirection * shootingForce, ForceMode.Impulse);
+        rb.AddForce(shootingDirection * shootingForce);
 
-        StartCoroutine(SelfDestruction(3f));
+        Debug.Log("Shooting Direction is " + shootingDirection);
     }
 
+    public void OnCollisionEnter()
+    {
+        StartCoroutine(SelfDestruction(.2f));
+
+    }
     IEnumerator SelfDestruction(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
